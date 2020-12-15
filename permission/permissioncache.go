d1f5eb5147717d40dc1966a765a093d9bda5dbcc
rxdn <29165304+rxdn@users.noreply.github.com>
2020-12-15 20:39:12 +0000

    Return errors

--- permission/permissioncache.go
+++ permission/permissioncache.go
@@ -9,17 +9,17 @@ import (
 
 const timeout = time.Minute * 5
 
-func GetCachedPermissionLevel(redis *redis.Client, guildId, userId uint64) (PermissionLevel, bool) {
+func GetCachedPermissionLevel(redis *redis.Client, guildId, userId uint64) (PermissionLevel, error) {
 	key := fmt.Sprintf("permissions:%d:%d", guildId, userId)
 	res, err := redis.Get(key).Result(); if err != nil {
-		return Everyone, false
+		return Everyone, err
 	}
 
 	parsed, err := strconv.Atoi(res); if err != nil {
-		return Everyone, false
+		return Everyone, err
 	}
 
-	return PermissionLevel(parsed), true
+	return PermissionLevel(parsed), nil
 }
 
 func SetCachedPermissionLevel(redis *redis.Client, guildId, userId uint64, level PermissionLevel) error {
--- permission/retriever.go
+++ permission/retriever.go
@@ -19,65 +19,67 @@ type Retriever interface {
 	GetGuildRoles(uint64) ([]guild.Role, error)
 }
 
-func GetPermissionLevel(retriever Retriever, member member.Member, guildId uint64) PermissionLevel {
+func GetPermissionLevel(retriever Retriever, member member.Member, guildId uint64) (PermissionLevel, error) {
 	// Check user ID in cache
-	if cached, found := GetCachedPermissionLevel(retriever.Redis(), guildId, member.User.Id); found {
-		return cached
+	if cached, err := GetCachedPermissionLevel(retriever.Redis(), guildId, member.User.Id); err == nil {
+		return cached, nil
+	} else if err != redis.Nil {
+		return Everyone, err
 	}
 
 	// Check if the user is a bot admin user
 	if retriever.IsBotAdmin(member.User.Id) {
-		return Admin
+		return Admin, nil
 	}
 
 	// Check if user is guild owner
 	guild, err := retriever.GetGuild(guildId)
 	if err == nil {
 		if member.User.Id == guild.OwnerId {
-			go SetCachedPermissionLevel(retriever.Redis(), guildId, member.User.Id, Admin)
-			return Admin
+			err := SetCachedPermissionLevel(retriever.Redis(), guildId, member.User.Id, Admin)
+			return Admin, err
 		}
 	}
 
 	// Check user perms for admin
 	adminUser, _ := retriever.Db().Permissions.IsAdmin(guildId, member.User.Id)
 	if adminUser {
-		go SetCachedPermissionLevel(retriever.Redis(), guildId, member.User.Id, Admin)
-		return Admin
+		err := SetCachedPermissionLevel(retriever.Redis(), guildId, member.User.Id, Admin)
+		return Admin, err
 	}
 
 	// Check roles from DB
 	adminRoles, _ := retriever.Db().RolePermissions.GetAdminRoles(guildId)
 	for _, adminRoleId := range adminRoles {
 		if member.HasRole(adminRoleId) {
-			go SetCachedPermissionLevel(retriever.Redis(), guildId, member.User.Id, Admin)
-			return Admin
+			err := SetCachedPermissionLevel(retriever.Redis(), guildId, member.User.Id, Admin)
+			return Admin, err
 		}
 	}
 
 	// Check if user has Administrator permission
 	hasAdminPermission := HasPermissions(retriever, guildId, member.User.Id, permission.Administrator)
 	if hasAdminPermission {
-		go SetCachedPermissionLevel(retriever.Redis(), guildId, member.User.Id, Admin)
-		return Admin
+		err := SetCachedPermissionLevel(retriever.Redis(), guildId, member.User.Id, Admin)
+		return Admin, err
 	}
 
 	// Check user perms for support
 	isSupport, _ := retriever.Db().Permissions.IsSupport(guildId, member.User.Id)
 	if isSupport {
-		go SetCachedPermissionLevel(retriever.Redis(), guildId, member.User.Id, Support)
-		return Support
+		err := SetCachedPermissionLevel(retriever.Redis(), guildId, member.User.Id, Support)
+		return Support, err
 	}
 
 	// Check DB for support roles
 	supportRoles, _ := retriever.Db().RolePermissions.GetSupportRoles(guildId)
 	for _, supportRoleId := range supportRoles {
 		if member.HasRole(supportRoleId) {
-			go SetCachedPermissionLevel(retriever.Redis(), guildId, member.User.Id, Support)
-			return Support
+			err := SetCachedPermissionLevel(retriever.Redis(), guildId, member.User.Id, Support)
+			return Support, err
 		}
 	}
 
-	go SetCachedPermissionLevel(retriever.Redis(), guildId, member.User.Id, Everyone)
-	return Everyone
+	err := SetCachedPermissionLevel(retriever.Redis(), guildId, member.User.Id, Everyone)
+	return Everyone, err
 }
