9a63dda9618ddde2eaf37c5dc879e4c914f24219
rxdn <29165304+rxdn@users.noreply.github.com>
2023-07-22 16:49:35 +0100

    Add environment type

--- observability/zapsentry.go
+++ observability/zapsentry.go
@@ -6,14 +6,26 @@ import (
 	"os"
 )
 
-func ZapSentryAdapter(environment string) func(core zapcore.Core) zapcore.Core {
+type Environment string
+
+func (e Environment) String() string {
+	return string(e)
+}
+
+const (
+	EnvironmentProduction  Environment = "production"
+	EnvironmentStaging     Environment = "staging"
+	EnvironmentDevelopment Environment = "development"
+)
+
+func ZapSentryAdapter(environment Environment) func(core zapcore.Core) zapcore.Core {
 	return func(core zapcore.Core) zapcore.Core {
 		return zapcore.RegisterHooks(core, func(entry zapcore.Entry) error {
 			if entry.Level == zapcore.ErrorLevel {
 				hostname, _ := os.Hostname()
 
 				sentry.CaptureEvent(&sentry.Event{
-					Environment: environment,
+					Environment: environment.String(),
 					Extra: map[string]any{
 						"caller": entry.Caller.String(),
 						"stack":  entry.Stack,
