744e5ece4b459070441c841167aced60aed6255b
rxdn <29165304+rxdn@users.noreply.github.com>
2024-06-13 00:15:37 +0100

    Create WithSpan funcs

--- sentry/performance.go
+++ sentry/performance.go
@@ -12,3 +12,23 @@ func StartTransaction(ctx context.Context, operation string, options ...sentry.S
 func StartSpan(ctx context.Context, operation string, options ...sentry.SpanOption) *sentry.Span {
 	return sentry.StartSpan(ctx, operation, options...)
 }
+
+func WithSpan0(ctx context.Context, operation string, f func(span *sentry.Span)) {
+	span := sentry.StartSpan(ctx, operation)
+	f(span)
+	span.Finish()
+}
+
+func WithSpan1[T any](ctx context.Context, operation string, f func(span *sentry.Span) T) T {
+	span := sentry.StartSpan(ctx, operation)
+	r1 := f(span)
+	span.Finish()
+	return r1
+}
+
+func WithSpan2[T any, U any](ctx context.Context, operation string, f func(span *sentry.Span) (T, U)) (T, U) {
+	span := sentry.StartSpan(ctx, operation)
+	r1, r2 := f(span)
+	span.Finish()
+	return r1, r2
+}
